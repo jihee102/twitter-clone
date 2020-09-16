d4d890cb82cc8a9677d29fe9f2527185b4012026
Jihee Hong <486480@student.saxion.nl>
2020-09-16 11:47:50 +0200

    Updates

---
+++ precache-manifest.c9b9d90a2717dc9cb16092f3d1752484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715c2275bdb0f6f1c32e5c82e6a8a4bb",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "46bf5251d7423a6ab2fa",
+    "url": "/twitter-clone/static/css/main.d280e784.chunk.css"
+  },
+  {
+    "revision": "91eca0dde44e2d1a393b",
+    "url": "/twitter-clone/static/js/2.9ad52ce5.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/twitter-clone/static/js/2.9ad52ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46bf5251d7423a6ab2fa",
+    "url": "/twitter-clone/static/js/main.e5735857.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
